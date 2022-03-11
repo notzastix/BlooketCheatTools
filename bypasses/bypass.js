bcbca8205670e6a69ca6f28f2005b83c0a668b10
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-11 03:11:54 -0600

    revert cause it didnt work lmao

--- bypasses/bypass.js
+++ bypasses/bypass.js
@@ -1,7 +1,18 @@
 /* credit to zastix for this bypass */
-window.alert = document.body.appendChild(document.createElement("iframe")).contentWindow.alert;
-window.prompt = document.body.appendChild(document.createElement("iframe")).contentWindow.prompt;
-window.confirm = document.body.appendChild(document.createElement("iframe")).contentWindow.confirm;
+window.alert = function(str) {
+    bypass = document.body.appendChild(document.createElement("iframe")).contentWindow.alert;
+    bypass(str)
+}
+window.prompt = function(str, s) {
+    bypass = document.body.appendChild(document.createElement("iframe")).contentWindow.prompt;
+    res = bypass(str, s)
+    return res;
+}
+window.confirm = function(str) {
+    bypass = document.body.appendChild(document.createElement("iframe")).contentWindow.confirm;
+    res = bypass(str)
+    return res;
+}
 setInterval(() => {
     if (document.getElementsByTagName("iframe")[1] != null) {
         document.getElementsByTagName("iframe")[1].remove();
