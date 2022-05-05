bee77cbaa4f13e6f23a09b928be20109f3292d4c
zastix <69748767+notzastix@users.noreply.github.com>
2022-05-05 15:46:26 -0500

    Create requestEncoder.js

---
+++ bypasses/requestEncoder.js
@@ -0,0 +1,9 @@
+var encoder = async (e, t) => {
+    let d = window.crypto.getRandomValues(new Uint8Array(12));
+    return window.btoa(Array.from(d).map(e => String.fromCharCode(e)).join("") + Array.from(new Uint8Array(await window.crypto.subtle.encrypt({
+        name: "AES-GCM",
+        iv: d
+    }, await window.crypto.subtle.importKey("raw", await window.crypto.subtle.digest("SHA-256", (new TextEncoder).encode(t)), {
+        name: "AES-GCM"
+    }, !1, ["encrypt"]), (new TextEncoder).encode(JSON.stringify(e))))).map(e => String.fromCharCode(e)).join(""))
+};
