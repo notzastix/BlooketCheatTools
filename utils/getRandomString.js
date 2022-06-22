9f769993fe8f472b1d036f42889037f99927e391
zastix <69748767+notzastix@users.noreply.github.com>
2022-06-21 22:10:06 -0500

    Delete utils directory

--- utils/getRandomString.js
+++
@@ -1,3 +0,0 @@
-/* thanks to https://github.com/charleywright my horrible coding now looks good. */
-const Strings = ["String0","String1","String2","String3","String4","String5","String6","String7","String8","String9","String10"];
-const getString = () => Strings[Math.floor(Math.random() * 11)];
